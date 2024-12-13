c9881e3f4d36b1858e3d88a454822878b1223e69
mark in oak <109883483+marquinhttps@users.noreply.github.com>
2024-12-13 09:24:04 -0300

    Update mysql.mjs

--- database/mysql.mjs
+++ database/mysql.mjs
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const conexao = new Sequelize({
+    "host": "dpg-cte1042lqhvc73db7upg-a",
+    "port": "5432",
     database: 'bdlpr2',
     username: 'root',
-    password: 'root',
-    dialect: 'mysql'
+    password: 'hiszmOIWdMz92jbbu9MZYWNrVZwF3aaC',
+    dialect: 'postgres'
 });
 
 export default conexao;
