24cc6db1f5bcc8c98c8c4e2c42cb8b77f3651c39
marquinhttps <carvalhomarcosryan@gmail.com>
2024-12-15 15:39:32 -0300

    criação de produtos.html

--- index.mjs
+++ index.mjs
@@ -11,3 +11,23 @@ app.use(express.static('views'));
 app.listen(80, function () {
     console.log('Na escuta.');
 });
+
+/*const express = require('express');
+const bodyParser = require('body-parser');
+const loginRoute = require('./routes/login');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Rotas
+app.use('/login', loginRoute);
+
+// Servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+*/
